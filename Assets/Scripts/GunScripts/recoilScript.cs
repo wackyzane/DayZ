1897c0f43c06bd649a57d888c01823c030d8ffd5
wackyzane <44377480+wackyzane@users.noreply.github.com>
2020-01-13 21:57:42 -0800

    Gun Recoil
    Finally got gun recoil to work how I wanted it too. Might change it in the future to be more responsive/jerky

---
+++ Assets/Scripts/GunScripts/recoilScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recoilScript : MonoBehaviour {
+
+    [SerializeField] private GameObject cam;
+    [SerializeField] private GameObject character;
+    [SerializeField] private AnimationCurve recoilCurve;
+
+    private float speed = 100f;
+
+    public void addRecoil(float recoil) {
+        StopCoroutine(gunRecoil(recoil));
+        StartCoroutine(gunRecoil(recoil));
+    }
+
+    private IEnumerator gunRecoil(float recoil) {
+        float lastNum = 0f;
+        float time = 0f;
+
+        do {
+            float num = recoilCurve.Evaluate(time);
+            num = num * recoil;
+            num = num - lastNum;
+            Vector3 marker = cam.transform.eulerAngles;
+            marker.x -= num;
+            cam.transform.eulerAngles = marker;
+            lastNum = num + lastNum;
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        } while (time < .5f);
+        
+        yield return null;
+    }
+}
--- Assets/Scripts/GunScripts/shootScript.cs
+++ Assets/Scripts/GunScripts/shootScript.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerShoot : MonoBehaviour {
-
+public class shootScript : MonoBehaviour
+{
     [SerializeField] private GameObject BulletPrefab;
     [SerializeField] private Transform barrel;
-
-    [SerializeField] private GameObject cam;
-    private cameraRotation cameraRotation;
 
     [SerializeField] private float bulletForce;
     [SerializeField] private float recoil;
@@ -16,40 +13,27 @@ public class playerShoot : MonoBehaviour {
     [SerializeField] private float magazineSize;
     private float currentMagazine;
 
-    [SerializeField] private Animator gunAim;
-    private bool isAimingGun = false;
+    [SerializeField] private recoilScript recoilScript;
 
     void Awake() {
         currentMagazine = magazineSize;
-        cameraRotation = cam.GetComponent<cameraRotation>();
     }
 
     void Update() {
-        if  (isAimingGun) {
-            cameraRotation.startGunSway();
-        }
-
-        if (Input.GetMouseButtonDown(1)) {
-            if (isAimingGun) {
-                isAimingGun = false;
-                gunAim.SetBool("RightClick", false);
-            } else {
-                isAimingGun = true;
-                gunAim.SetBool("RightClick", true);
-            }
-        }
-        
         if (Input.GetMouseButtonDown(0)) {
             if (currentMagazine > 0) {
                 GameObject spawn = Instantiate(BulletPrefab, barrel.transform.position, Quaternion.identity);
                 Rigidbody spawnRb = spawn.GetComponent<Rigidbody>();
                 spawnRb.AddForce(transform.forward * bulletForce);
-                cameraRotation.gunRecoil(recoil);
+                // Add recoil
+                recoilScript.addRecoil(recoil);
                 currentMagazine -= 1;
             } else {
                 Debug.Log("Click, Click, Click");
             }
         }
+
+        // Reload, Add Customize Reload Button
         if (Input.GetKey("r")) {
             currentMagazine = magazineSize;
             Debug.Log(currentMagazine);
